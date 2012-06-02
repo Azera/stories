fd8daed1b06881990252a9008cb5d6e39c75f894
Azera <github@jocosoft.com>
2012-06-02 16:20:33 -0500

    Fixed login, prevented new and inactive accounts from logging in, added stale account reactivation, etc.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -8,6 +8,29 @@
 class UserIdentity extends CUserIdentity
 {
 	/**
+	 * Reasons a login can fail....
+	 */
+	const ERROR_ACCOUNT_NEW = 4;
+	const ERROR_ACCOUNT_LOCKED = 5;
+	const ERROR_ACCOUNT_INACTIVE = 6;
+	const ERROR_ACCOUNT_TEMPLOCK = 7;
+	const ERROR_ACCOUNT_STALE = 8;
+
+	/**
+	 * If a user has not logged in for over this amount of time, they
+	 * will have to re-activate their account.
+	 * 180 days = 180*24*60*60 = 15552000
+	 */
+	const TIME_ACCOUNT_STALE = 15552000;
+
+	/**
+	 * If a user fails authentication 3 time in a row, their account
+	 * will be locked for this amount of time.
+	 * 3 hours = 3*60*60 = 10800
+	 */
+	const TIME_ACCOUNT_LOCKOUT = 10800;
+
+	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
 	 * are both 'demo'.
@@ -17,17 +40,104 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		$user = User::model()->findByAttributes(array(
+			'username' => $this->username,
+		));
+		if (NULL == $user)
+		{
+			$this->errorCode = self::ERROR_PASSWORD_INVALID;
+			return false;
+		}
+
+		// Convert timestamps to integers so we can calculate with them
+		$now = time();
+		// This is "3 hours from now"
+		$newLockTime = time() + self::TIME_ACCOUNT_LOCKOUT;
+		// This is "180 days ago"
+		$staleTime = time() - self::TIME_ACCOUNT_STALE;
+
+		// Expire lock
+		if ((NULL != $user->locked_until) && (strtotime($user->locked_until) <= $now))
+		{
+			$user->login_failures = 0;
+			$user->locked_until = NULL;
+		}
+
+		$isStale = (NULL != $user->lastlogin_time) && (strtotime($user->lastlogin_time) < $staleTime);
+		$isLocked = (NULL != $user->locked_until);
+
+		// A new account can not login. ever
+		if (User::STATUS_NEW == $user->status_id)
+			$this->errorCode = self::ERROR_ACCOUNT_NEW;
+
+		// Same for banned account
+		elseif (User::STATUS_LOCKED == $user->status_id)
+			$this->errorCode = self::ERROR_ACCOUNT_LOCKED;
+
+		// Same for otherwise inactive accounts
+		elseif (User::STATUS_ACTIVE != $user->status_id)
+			$this->errorCode = self::ERROR_ACCOUNT_INACTIVE;
+
+		// If account is templocked, we reset the lock timer
+		elseif ($isLocked)
+		{
+			$user->locked_until = date("Y-m-d H:i:s", $newLockTime);
+			$this->errorCode = self::ERROR_ACCOUNT_TEMPLOCK;
+		}
+
+		// If password is invalid, we increase the counter.
+		// when it reaches 3, we lock the account
+		elseif (User::encrypt($user->salt . $this->password) !== $user->password)
+		{
+			$user->login_failures++;
+			if ($user->login_failures >= 3)
+			{
+				$user->locked_until = date("Y-m-d H:i:s", $newLockTime);
+			}
+			$this->errorCode = self::ERROR_PASSWORD_INVALID;
+		}
+
+		// Ok, password is crrect. Now we only need to check if the
+		// account has been stale for more than 180 days.
+		elseif ($isStale)
+		{
+			$unsaltedkey = User::randomString();
+			$user->activkey = User::encrypt($user->salt . $unsaltedkey);
+			$user->status_id = User::STATUS_INACTIVE;
+			// We also need to clear the login date to prevent an
+			// "endless loop"
+			$user->lastlogin_time = NULL;
+
+			// Send activation email with the unsalted $activkey
+			$act_url = Yii::app()->createAbsoluteUrl('/account/activate', array(
+				'user' => $user->username,
+				'key' => $unsaltedkey
+					));
+
+			$adminEmail = Yii::app()->params['adminEmail'];
+			$headers = "MIME-Version: 1.0\r\nFrom: $adminEmail\r\nReply-To: $adminEmail\r\nContent-Type: text/html; charset=utf-8";
+			$subject = 'Account re-activation for ' . Yii::app()->name;
+			$message = "Please re-activate your account by clicking on the following link:\r\n$act_url";
+
+			mail(
+					$user->email, '=?UTF-8?B?' . base64_encode($subject) . '?=', str_replace("\n.", "\n..", wordwrap($message, 70)), $headers
+			);
+
+			$this->errorCode = self::ERROR_ACCOUNT_STALE;
+		}
+
+		// Rests nothing but to accept the login
 		else
-			$this->errorCode=self::ERROR_NONE;
+		{
+			$user->login_failures = 0;
+			$user->locked_until = NULL;
+			$user->activkey = NULL;
+			$user->lastlogin_time = new CDbExpression('NOW()');
+			$this->errorCode = self::ERROR_NONE;
+		}
+
+		$user->save();
 		return !$this->errorCode;
 	}
+
 }
--- protected/models/User.php
+++ protected/models/User.php
@@ -20,169 +20,168 @@
 class User extends CActiveRecord
 {
 
-    // This holds the unsalted key that is required to activate a
-    // new account
-    protected $unsaltedkey = NULL;
+	// This holds the unsalted key that is required to activate a
+	// new account
+	protected $unsaltedkey = NULL;
 
-    const STATUS_NEW        = 1; // New account
-    const STATUS_LOCKED     = 2; // Banned account
-    const STATUS_INACTIVE   = 3; // Stale account to be re-activated
-    const STATUS_ACTIVE     = 9; // Active account
+	const STATUS_NEW		= 1; // New account
+	const STATUS_LOCKED		= 2; // Banned account
+	const STATUS_INACTIVE	= 3; // Stale account to be re-activated
+	const STATUS_ACTIVE		= 9; // Active account
 
-    /**
-     * Returns the static model of the specified AR class.
-     * @param string $className active record class name.
-     * @return User the static model class
-     */
-    public static function model($className = __CLASS__)
-    {
-        return parent::model($className);
-    }
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return User the static model class
+	 */
+	public static function model($className = __CLASS__)
+	{
+		return parent::model($className);
+	}
 
-    /**
-     * @return string the associated database table name
-     */
-    public function tableName()
-    {
-        return 'user';
-    }
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'user';
+	}
 
-    /**
-     * @return array validation rules for model attributes.
-     */
-    public function rules()
-    {
-        // NOTE: you should only define rules for those attributes that
-        // will receive user inputs.
-        return array(
-            array('username, email', 'required'),
-            array('username', 'match', 'pattern' => '/^[A-Za-z][A-Za-z0-9_]+$/u', 'message' => 'Invalid username. Username must start with a letter, and only contain letters and numbers.'),
-            array('username', 'length', 'min' => 5, 'max' => 20),
-            array('username, email', 'unique'),
-            array('email', 'email'),
-        );
-    }
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('username, email', 'required'),
+			array('username', 'match', 'pattern' => '/^[A-Za-z][A-Za-z0-9_]+$/u', 'message' => 'Invalid username. Username must start with a letter, and only contain letters and numbers.'),
+			array('username', 'length', 'min' => 5, 'max' => 20),
+			array('username, email', 'unique'),
+			array('email', 'email'),
+		);
+	}
 
-    /**
-     * @return array relational rules.
-     */
-    public function relations()
-    {
-        // NOTE: you may need to adjust the relation name and the related
-        // class name for the relations automatically generated below.
-        return array(
-        );
-    }
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
 
-    /**
-     * @return array customized attribute labels (name=>label)
-     */
-    public function attributeLabels()
-    {
-        return array(
-            'id' => 'ID',
-            'username' => 'Username',
-            'email' => 'Email',
-            'status_id' => 'Status',
-            'salt' => 'Salt',
-            'password' => 'Password',
-            'activkey' => 'Activkey',
-            'login_failures' => 'Login failures',
-            'locked_until' => 'Locked until',
-            'create_time' => 'Create Time',
-            'lastlogin_time' => 'Lastlogin Time',
-            'is_admin' => 'Is Admin',
-        );
-    }
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'username' => 'Username',
+			'email' => 'Email',
+			'status_id' => 'Status',
+			'salt' => 'Salt',
+			'password' => 'Password',
+			'activkey' => 'Activkey',
+			'login_failures' => 'Login failures',
+			'locked_until' => 'Locked until',
+			'create_time' => 'Create Time',
+			'lastlogin_time' => 'Lastlogin Time',
+			'is_admin' => 'Is Admin',
+		);
+	}
 
-    /**
-     * Retrieves a list of models based on the current search/filter conditions.
-     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-     */
-    public function search()
-    {
-        // Warning: Please modify the following code to remove attributes that
-        // should not be searched.
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
 
-        $criteria = new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-        $criteria->compare('username', $this->username, true);
-        $criteria->compare('email', $this->email, true);
+		$criteria->compare('username', $this->username, true);
+		$criteria->compare('email', $this->email, true);
 
-        return new CActiveDataProvider($this, array(
-                    'criteria' => $criteria,
-                ));
-    }
+		return new CActiveDataProvider($this, array(
+			'criteria' => $criteria,
+		));
+	}
 
-    public function scopes()
-    {
-        return array(
-            'active' => array(
-                'condition' => 'status_id=' . self::STATUS_ACTIVE,
-            ),
-            'administrators' => array(
-                'condition' => 'is_admin=1',
-            ),
-        );
-    }
+	public function scopes()
+	{
+		return array(
+			'active' => array(
+				'condition' => 'status_id=' . self::STATUS_ACTIVE,
+			),
+			'administrators' => array(
+				'condition' => 'is_admin=1',
+			),
+		);
+	}
 
-    public static function getOptions($id, $value = NULL)
-    {
-        $options = array(
-            'is_admin' => array(
-                '0' => 'No',
-                '1' => 'Yes',
-            ),
-            'status_id' => array(
-                self::STATUS_NEW        => 'New',
-                self::STATUS_LOCKED     => 'Locked',
-                self::TATUS_INACTIVE    => 'Inactive',
-                self::STATUS_ACTIVE     => 'Active',
-            ),
-        );
+	public static function getOptions($id, $value = NULL)
+	{
+		$options = array(
+			'is_admin' => array(
+				'0' => 'No',
+				'1' => 'Yes',
+			),
+			'status_id' => array(
+				self::STATUS_NEW		=> 'New',
+				self::STATUS_LOCKED		=> 'Locked',
+				self::TATUS_INACTIVE	=> 'Inactive',
+				self::STATUS_ACTIVE		=> 'Active',
+			),
+		);
 
-        if (!isset($options[$id]))
-            throw new CException('Invalid option id "' . $id . '" specified');
-        if ($value == NULL)
-            return $options[$id];
-        if (!isset($options[$id][$value]))
-            throw new CException('Invalid value "' . $value . '" specified for option "' . $id . '"');
-        return $options[$id][$value];
-    }
+		if (!isset($options[$id]))
+			throw new CException('Invalid option id "' . $id . '" specified');
+		if ($value == NULL)
+			return $options[$id];
+		if (!isset($options[$id][$value]))
+			throw new CException('Invalid value "' . $value . '" specified for option "' . $id . '"');
+		return $options[$id][$value];
+	}
 
-    /**
-     * Instead of inheriting from AuditActiveRecord, we implement our own,
-     * because only have one field to take care of, instead of 4
-     */
-    protected function beforeValidate()
-    {
-        if ($this->isNewRecord)
-        {
-            $this->create_time = new CDbExpression('NOW()');
-            $this->status_id = self::STATUS_NEW;
-        }
-        return parent::beforeValidate();
-    }
+	/**
+	 * Instead of inheriting from AuditActiveRecord, we implement our own,
+	 * because only have one field to take care of, instead of 4
+	 */
+	protected function beforeValidate()
+	{
+		if ($this->isNewRecord)
+		{
+			$this->create_time = new CDbExpression('NOW()');
+			$this->status_id = self::STATUS_NEW;
+		}
+		return parent::beforeValidate();
+	}
 
-    /**
-     * @return encrypted string.
-     */
-    public static function encrypt($string = '')
-    {
-        return md5($string);
-    }
+	/**
+	 * @return encrypted string.
+	 */
+	public static function encrypt($string = '')
+	{
+		return md5($string);
+	}
 
-    // A function to generate a salt value and activation code
-    public static function randomString($length = 32)
-    {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $return = '';
-        $totalChars = strlen($characters) - 1;
-        for ($i = 0; $i < 32; ++$i)
-        {
-            $return .= $characters[rand(0, $totalChars)];
-        }
-        return $return;
-    }
-
+	// A function to generate a salt value and activation code
+	public static function randomString($length = 32)
+	{
+		$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		$return = '';
+		$totalChars = strlen($characters) - 1;
+		for ($i = 0; $i < 32; ++$i)
+		{
+			$return .= $characters[rand(0, $totalChars)];
+		}
+		return $return;
+	}
 }
