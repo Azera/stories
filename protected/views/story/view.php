20183a9770c8188326b50d411775acb3d08660bc
Azera <github@jocosoft.com>
2012-06-05 15:09:36 -0500

    Added some styles to comment section

--- protected/views/story/view.php
+++ protected/views/story/view.php
@@ -12,7 +12,7 @@ Posted on <?php echo Yii::app()->dateFormatter->formatDateTime($model->create_ti
 <?php echo CHtml::encode($model->description); ?>
 <hr>
 <?php
-	// No need for encoding here. String as halready been encoded before parsed
+	// No need for encoding here. String has already been encoded before parsed
 	echo $model->content_html;
 ?>
 <hr>
@@ -21,23 +21,32 @@ Rating gadget, fixed if user already rated this story<br/>
 <?php
 	echo CHtml::ajaxLink('Favorite story', array('story/favorite')) .', '.
 		 CHtml::ajaxLink('Favorite author', array('user/favorite', 'id'=>$model->author->id));
-	if(NULL === $model->reviewed_user) echo ', '. CHtml::link('Report this story.', array('story/report', 'id'=>$model->id));
+	if($model->canBeReported) echo ', '. CHtml::link('Report story.', array('story/report', 'id'=>$model->id));
 ?><br />
 
 <h2>Comments</h2>
 <?php if(count($model->comments) == 0): ?>
 	<p>No comments left yet.</p>
 <?php else: ?>
+	<div class="commentlist">
 	<?php foreach($model->comments as $comment): ?>
 		<div class="comment">
-			<div class="commentheader">
-				<?php echo Yii::app()->dateFormatter->formatDateTime($comment->create_time,'medium',null); ?> by <?php echo CHtml::link(CHtml::encode($comment->author->username), array('user/view', 'id'=>$comment->author->id)); ?>
+			<div class="comment-header">
+				<img class="comment-image" src="/image/user-<?php echo $comment->author->id; ?>.jpg" alt="" />
+				<div class="comment-links">
+					<?php if($comment->canBeReported): ?>
+						<?php echo CHtml::link('Report comment', array('comment/report', 'id'=>$comment->id)); ?><br />
+					<?php endif; ?>
+				</div>
+				<?php echo CHtml::link(CHtml::encode($comment->author->username), array('user/view', 'id'=>$comment->author->id), array('class'=>'comment-author')); ?><br />
+				<span class="comment-date"><?php echo Yii::app()->dateFormatter->formatDateTime($comment->create_time,'medium',null); ?></span>
 			</div>
-			<div class="commentbody">
+			<div class="comment-text">
 				<?php echo CHtml::encode($comment->content); ?>
 			</div>
 		</div>
 	<?php endforeach; ?>
+	</div>
 <?php endif; ?>
 
 <h3>Leave a comment</h3>
